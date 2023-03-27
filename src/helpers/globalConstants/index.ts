5030da7cfdc20fd30cf28f0a3c2b23661fb83d31
delfina <delfifenocchio@gmail.com>
2023-03-27 00:05:56 -0300

    ✨ add images sizes in global constants

---
+++ src/helpers/globalConstants/index.ts
@@ -0,0 +1,17 @@
+export const IMAGE_SIZES = [
+  {
+    id: 1,
+    width: 120,
+    height: 120,
+  },
+  {
+    id: 2,
+    width: 160,
+    height: 120,
+  },
+  {
+    id: 3,
+    width: 400,
+    height: 300,
+  },
+];
